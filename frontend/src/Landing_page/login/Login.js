12e23bd3757b6257d1dea459584b0a04b66a65d2
Aditya Agarwal <aditya2004lawrencian@gmail.com>
2025-09-16 19:44:42 +0530

    dashboard link updated

--- frontend/src/Landing_page/login/Login.js
+++ frontend/src/Landing_page/login/Login.js
@@ -10,7 +10,7 @@ function Login() {
     e.preventDefault();
     setMessage('');
     try {
-      const response = await fetch('http://localhost:3002/login', {
+      const response = await fetch('https://zerodha-clone-dashboard-gucq.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
